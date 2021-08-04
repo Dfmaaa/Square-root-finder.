e9e7b2dbcfe9800cc0e853c9ab2482de40a48657
Sameer Achhab <sameerkw23@gmail.com>
2021-08-04 08:49:30 +0530

    Update square root.py

--- square root.py
+++ square root.py
@@ -2,7 +2,7 @@ print("Type a number and this application will find the square root.")
 print("This application was made by Sameer Achhab.")
 import math
 while True:
- num=int(input())
+ num=float(input())
  answer=math.sqrt(num)
  print(answer)
 
